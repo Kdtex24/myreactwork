6cb5be017c935f3c3e7320c6a59d4a1fb70bbc1c
kedeayei okoi <39980477+Kdtex24@users.noreply.github.com>
2023-10-14 09:55:43 +0100

    Add files via upload

---
+++ src/components/LightMode/LightMode.stories.js
@@ -0,0 +1,16 @@
+import { LightMode } from ".";
+
+export default {
+  title: "Components/LightMode",
+  component: LightMode,
+};
+
+export const Default = {
+  args: {
+    className: {},
+    overlapGroupClassName: {},
+    rectangleClassName: {},
+    logoClassName: {},
+    logo: "/img/logo-2.png",
+  },
+};
